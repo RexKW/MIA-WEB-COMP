c96c0ea70fe85b94237cfa4b13663aea46fd91b4
Verzional <verzional777@gmail.com>
2025-11-16 12:10:46 +0700

    Responsive Pages

--- src/app/(app)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from "@/components/navigation/sidebar";
-
-export default function AppLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <Sidebar>{children}</Sidebar>;
-}
